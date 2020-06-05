bf70ebf2cebb90e1ff28d9b3037c44f88e45bcf7
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2020-06-04 23:43:09 -0300

    Adicionada função para transformar imagem gif ou jpg em base64, no programa createDataBase!

--- Swing/src/main/java/br/edu/ifpb/createDataBase.java
+++ Swing/src/main/java/br/edu/ifpb/createDataBase.java
@@ -1,5 +1,8 @@
 package br.edu.ifpb;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -8,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,14 +22,16 @@ public class createDataBase {
     public static void main(String[] args) throws IOException {
         List<List<String>> nomes = getAlternativas(path);
         List<String> nomes1 = getQuestoes(path);
-        for (List<String> s : nomes) {
+        List<List<String>> images = getImagensInBase64(path);
+        for (List<String> s : images) {
             for (String string : s) {
                 System.out.println(string);
             }
             System.out.println("-----------------------------------------------");
         }
-        System.out.println(nomes.size());
-        System.out.println(nomes1);
+//        System.out.println(nomes.size());
+//        System.out.println(nomes1);
+//        System.out.println(images);
     }
 
     public static List<List<String>> getAlternativas(Path path) {
@@ -41,25 +47,27 @@ public class createDataBase {
                     for (int i = 0; i < html.size(); i ++) {
                         StringBuilder alternativa = new StringBuilder();
                         if (html.get(i).contains(">a)") || html.get(i).contains(">b)") || html.get(i).contains(">c)") || html.get(i).contains(">d)") || html.get(i).contains(">e)")) {
-                            for (int j = i - 1; j < html.size(); j ++) {
-                                alternativa.append(html.get(j));
-                                if (html.get(j).contains("</p>")) {
-                                    alternativas.add(alternativa.toString());
-                                    break;
+                            if (html.get(i).contains("<p")) {
+                                for (int j = i; j < html.size(); j ++) {
+                                    alternativa.append(html.get(j));
+                                    if (html.get(j).contains("</p>")) {
+                                        alternativas.add(alternativa.toString());
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                        else if (html.get(i).contains("<p") && (html.get(i).contains(">a)") || html.get(i).contains(">b)") || html.get(i).contains(">c)") || html.get(i).contains(">d)") || html.get(i).contains(">e)"))) {
-                            for (int j = i; j < html.size(); j ++) {
-                                alternativa.append(html.get(j));
-                                if (html.get(j).contains("</p>")) {
-                                    alternativas.add(alternativa.toString());
-                                    break;
+                            else {
+                                for (int j = i - 1; j < html.size(); j ++) {
+                                    alternativa.append(html.get(j));
+                                    if (html.get(j).contains("</p>")) {
+                                        alternativas.add(alternativa.toString());
+                                        break;
+                                    }
                                 }
                             }
                         }
                     }
-                    if (alternativas.size() > 1) alternativasArray.add(alternativas);
+                    if (alternativas.size() > 0) alternativasArray.add(alternativas);
                     alternativas = new ArrayList<>();
                 }
             }
@@ -80,5 +88,25 @@ public class createDataBase {
             }
         }
         return nomes;
+    }
+
+    public static List<List<String>> getImagensInBase64(Path path) throws IOException {
+        List<List<String>> imagens = new ArrayList<>();
+        DirectoryStream<Path> arquivos = Files.newDirectoryStream(path);
+        List<String> base64 = new ArrayList<>();
+        for (Path p : arquivos) {
+            if (Files.isDirectory(p) && (! p.getFileName().toString().contains("Q"))) imagens.addAll(getImagensInBase64(p));
+            else if (p.getFileName().toString().contains("gif") || p.getFileName().toString().contains("jpg")) {
+                // System.out.println(p.toString());
+                BufferedImage bufferedImage = ImageIO.read(new File(p.toString()));
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ImageIO.write(bufferedImage, p.getFileName().toString().substring(p.getFileName().toString().length() - 3), byteArrayOutputStream);
+                byte[] dataBytes = byteArrayOutputStream.toByteArray();
+                String encoded = Base64.getEncoder().encodeToString(dataBytes);
+                base64.add(p.getFileName().toString().substring(p.getFileName().toString().length() - 3) + encoded);
+            }
+        }
+        if (base64.size() > 0) imagens.add(base64);
+        return imagens;
     }
 }
--- Swing/src/main/java/br/edu/ifpb/test1.java
+++ Swing/src/main/java/br/edu/ifpb/test1.java
@@ -29,21 +29,21 @@ public class test1 {
         // System.out.println(docx.getTables());
         // System.out.println(aux);
         // stringToMongoDB(aux);
-        extractImages(docx);
-//        BufferedImage bImage = ImageIO.read(new File("image20.wmf")); // conversão entre imagem e byte
-//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//        ImageIO.write(bImage, "x-wmf", bos );
-//        byte [] data = bos.toByteArray();
-//
-//        String encoded = Base64.getEncoder().encodeToString(data); // conversão entre byte pra base64
-//        System.out.println(encoded);
-//
-//        byte[] decoded = Base64.getDecoder().decode(encoded); // conversão entre base64 pra byte
-//
-//        ByteArrayInputStream bis = new ByteArrayInputStream(decoded);
-//        BufferedImage bImage2 = ImageIO.read(bis);
-//        ImageIO.write(bImage2, "jpg", new File("output.jpg") ); // conversão byte pra imagem
-//        System.out.println("image created");
+//        extractImages(docx);
+        BufferedImage bImage = ImageIO.read(new File("image001.gif")); // conversão entre imagem e byte
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(bImage, "gif", bos );
+        byte [] data = bos.toByteArray();
+
+        String encoded = Base64.getEncoder().encodeToString(data); // conversão entre byte pra base64
+        System.out.println(encoded);
+
+        byte[] decoded = Base64.getDecoder().decode(encoded); // conversão entre base64 pra byte
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(decoded);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, "gif", new File("output.gif") ); // conversão byte pra imagem
+        System.out.println("image created");
     }
 
     // ByteArrayInputStream = buffer de array de bytes na mémoria
@@ -84,34 +84,34 @@ public class test1 {
         }
     }
 
-    private static void stringToMongoDB(List<String> questoes) {
-        List<List<String>> questoesMatriz = new ArrayList<>();
-        List<String> aux = new ArrayList<>();
-        for (int i = 0; i < questoes.size(); i ++) {
-            if (questoes.get(i).contains("Questão") && i != 0) {
-                questoesMatriz.add(aux);                         // transforma a lista de string do docx, em uma matriz, uma lista de questões, onde cada questão é uma lista
-                aux = new ArrayList<>();
-            }
-            if (questoes.get(i).length() > 2) aux.add(questoes.get(i));
-        }
-
-        for (List<String> I : questoesMatriz) {
-            for (String i : I) System.out.println(i);
-            System.out.println();
-        }
-        List<Question> questionList = new ArrayList<>(); // vai percorrendo a matriz, e passando as strings das sublistas para um objeto questão
-        for (List<String> questao : questoesMatriz) {
-            List<String> texto = new ArrayList<>();
-            String cab = questao.get(questao.size() - 1);
-            String nome = questao.get(0);
-            List<String> alternativas = new ArrayList<>();
-            for (int i = 1; i < questao.size(); i ++) {
-                if (questao.get(i).charAt(1) == ')') alternativas.add(questao.get(i));
-                else texto.add(questao.get(i));
-            }
-            // questionList.add(new Question(nome, texto, alternativas));
-        }
-        for (Question i : questionList) System.out.println(i);
-    }
+//    private static void stringToMongoDB(List<String> questoes) {
+//        List<List<String>> questoesMatriz = new ArrayList<>();
+//        List<String> aux = new ArrayList<>();
+//        for (int i = 0; i < questoes.size(); i ++) {
+//            if (questoes.get(i).contains("Questão") && i != 0) {
+//                questoesMatriz.add(aux);                         // transforma a lista de string do docx, em uma matriz, uma lista de questões, onde cada questão é uma lista
+//                aux = new ArrayList<>();
+//            }
+//            if (questoes.get(i).length() > 2) aux.add(questoes.get(i));
+//        }
+//
+//        for (List<String> I : questoesMatriz) {
+//            for (String i : I) System.out.println(i);
+//            System.out.println();
+//        }
+//        List<Question> questionList = new ArrayList<>(); // vai percorrendo a matriz, e passando as strings das sublistas para um objeto questão
+//        for (List<String> questao : questoesMatriz) {
+//            List<String> texto = new ArrayList<>();
+//            String cab = questao.get(questao.size() - 1);
+//            String nome = questao.get(0);
+//            List<String> alternativas = new ArrayList<>();
+//            for (int i = 1; i < questao.size(); i ++) {
+//                if (questao.get(i).charAt(1) == ')') alternativas.add(questao.get(i));
+//                else texto.add(questao.get(i));
+//            }
+//            // questionList.add(new Question(nome, texto, alternativas));
+//        }
+//        for (Question i : questionList) System.out.println(i);
+//    }
 
 }
