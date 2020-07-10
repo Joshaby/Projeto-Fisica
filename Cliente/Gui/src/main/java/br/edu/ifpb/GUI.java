21586eba86797ba9a7a15ccf328ff7e22b236fc9
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2020-07-10 12:03:12 -0300

    Adicionada função para exibir questão aberta!

--- Cliente/Gui/src/main/java/br/edu/ifpb/GUI.java
+++ Cliente/Gui/src/main/java/br/edu/ifpb/GUI.java
@@ -1,8 +1,9 @@
 package br.edu.ifpb;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.event.*;
 import java.awt.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -20,8 +21,7 @@ public class GUI extends JFrame {
     private JPanel panel; // painel principal, contêm todos os outros paineis
     private JPanel panel1; // painel do headbar
     private JPanel panel2; // painel do editorPane, onde será mostrado o HTML, e da entrada de respostas, seja digitada ou selecionada
-    private JPanel panel4; // painel das alternativas
-    private JPanel panel5;
+    private JPanel panel3; // painel das alternativas
     private JLabel icon; // ícone do app
     private JLabel appName; // nome do Aplicativo
     private JLabel time; // tempo restante para responder a questão
@@ -30,9 +30,11 @@ public class GUI extends JFrame {
     private JEditorPane editorPane; // painel de exibição do HTML
     private JScrollPane scrollPane; // scroll bar do editrPane
     private ButtonGroup buttonGroup;
+    private JTextArea answerEntry;
     private List<JRadioButton> radioButtonList;
     private int currentQuestionPosi = 1;
     private int maxQuestionPosi;
+    private boolean isMultipleChoiceQuestion;
 
     public GUI () throws IOException {
         try {
@@ -45,22 +47,7 @@ public class GUI extends JFrame {
             maxQuestionPosi = serverConnection.getQuestionAmout();
             buttonGroup = new ButtonGroup();
             radioButtonList = new ArrayList<>();
-            panel = new JPanel() {
-                @Override
-                protected void paintComponent(Graphics g) {
-                    super.paintComponent(g);
-                    Dimension arcs = new Dimension(15,15);
-                    int width = getWidth();
-                    int height = getHeight();
-                    Graphics2D graphics = (Graphics2D) g;
-                    graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    //Draws the rounded opaque panel with borders.
-                    graphics.setColor(getBackground());
-                    graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);//paint background
-                    graphics.setColor(getForeground());
-                    graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);//paint border
-                }
-            };
+            panel = new JPanel();
             panel.setBounds(10,10,100,30);
             panel.setOpaque(false);
             panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -68,10 +55,10 @@ public class GUI extends JFrame {
             panel1.setOpaque(false);
             panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
             panel2 = new JPanel();
-            panel2.setOpaque(false);
             panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS));
-            panel4 = new JPanel();
-            panel4.setLayout(new BoxLayout(panel4, BoxLayout.Y_AXIS));
+            panel2.setOpaque(false);
+            panel3 = new JPanel();
+            panel3.setOpaque(false);
 
             // Painel1 e seus componentes
 
@@ -100,21 +87,14 @@ public class GUI extends JFrame {
             editorPane = new JEditorPane();
             editorPane.setEditable(false);
             scrollPane = new JScrollPane(editorPane);
-//            scrollPane.setMaximumSize(new Dimension(830, 600));
-//            scrollPane.setMinimumSize(new Dimension(830, 600));
-            scrollPane.setPreferredSize(new Dimension(830, 600));
-            scrollPane.setBorder(new EmptyBorder( 0, 0, 0, 0));
-            panel2.add(Box.createRigidArea(new Dimension(15, 15)));
-            panel2.add(scrollPane);
-            panel2.add(Box.createRigidArea(new Dimension(15, 15)));
 
             //
 
-            panel.add(Box.createRigidArea(new Dimension(0, 5)));
+            panel.add(Box.createRigidArea(new Dimension(0, 10)));
             panel.add(panel1);
-            panel.add(Box.createRigidArea(new Dimension(5, 5)));
+            panel.add(Box.createRigidArea(new Dimension(10, 10)));
             panel.add(panel2);
-            panel.add(Box.createRigidArea(new Dimension(5, 5)));
+            panel.add(Box.createRigidArea(new Dimension(10, 10)));
             add(panel);
             createQuestionComponents();
         }
@@ -127,35 +107,59 @@ public class GUI extends JFrame {
         List<String> alternatives = new ArrayList<>();
         String id = serverConnection.getQuestion(alternatives);
         if (id == null) finalPanel();
-        if (alternatives.isEmpty()) createEssayQuestionComponents();
-        else createMultipleChoiceQuestionComponents(alternatives, id);
+        if (alternatives.isEmpty()) {
+            createEssayQuestionComponents(id);
+            isMultipleChoiceQuestion = false;
+        }
+        else {
+            createMultipleChoiceQuestionComponents(alternatives, id);
+            isMultipleChoiceQuestion = true;
+        }
     }
 
-    public void updateQuestionComponents() {
-//        JPanel panel5 = new JPanel();
-//        panel5.setLayout(new BoxLayout(panel5, BoxLayout.X_AXIS));
-//        JTextArea textArea = new JTextArea();
-//        textArea.setBorder(new LineBorder(Color.BLACK, 1, true));
-//        textArea.setMaximumSize(new Dimension(200, 20));
-//        textArea.setMinimumSize(new Dimension(200, 20));
-//        panel2.add(scrollPane);
-//        panel2.add(Box.createRigidArea(new Dimension(18, 18)));
-//        panel5.add(new JLabel("Digite aqui sua respotas(apenas números)!"));
-//        panel5.add(Box.createRigidArea(new Dimension(10, 10)));
-//        panel5.add(textArea);
-//        panel2.add(panel5);
-//        panel2.setOpaque(false);
-    }
-    public void createEssayQuestionComponents() {
-
+    public void createEssayQuestionComponents(String id) throws IOException {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
+        editorPane.setPage(new File(".cache" + SEPARATOR + "Q" + id + ".HTM").toURI().toURL());
+        scrollPane.setPreferredSize(new Dimension(1300, 550));
+        answerEntry = new JTextArea();
+        CaretListener listener = new CaretListener() {
+            public void caretUpdate(CaretEvent caretEvent) {
+                JTextArea r = (JTextArea) caretEvent.getSource();
+                if (r.getText().equals("1")) {
+                    nextButton.setEnabled(false);
+                    System.out.println("oi");
+                }
+                else nextButton.setEnabled(true);
+            }
+        };
+        answerEntry.addCaretListener(listener);
+        answerEntry.setBorder(new LineBorder(Color.BLACK, 1, true));
+        answerEntry.setMaximumSize(new Dimension(200, 20));
+        answerEntry.setMinimumSize(new Dimension(200, 20));
+        panel1.add(new JLabel("Digite aqui sua respotas(apenas números)!"));
+        panel1.add(Box.createRigidArea(new Dimension(10, 10)));
+        panel1.add(answerEntry);
+        panel.add(scrollPane);
+        panel.add(Box.createRigidArea(new Dimension(30, 30)));
+        panel.add(panel1);
+        panel.add(Box.createRigidArea(new Dimension(10, 10)));
+        panel3.add(panel);
+        panel2.add(panel3);
+        SwingUtilities.updateComponentTreeUI(this);
     }
     public void createMultipleChoiceQuestionComponents(List<String> alternatives, String id) throws IOException {
-        panel5 = new JPanel();
-        panel5.setLayout(new BoxLayout(panel5, BoxLayout.Y_AXIS));
-        panel5.setOpaque(false);
+        radioButtonList = new ArrayList<>();
+        JPanel panel = new JPanel(); // painel principal com área de texto e painel das alternativas
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        JPanel panel1 = new JPanel(); // painel das alternativas
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        scrollPane.setPreferredSize(new Dimension(830, 600));
         int amountOfPanels = alternatives.size();
+        editorPane.setPage(new File(".cache" + SEPARATOR + "Q" + id + ".HTM").toURI().toURL());
         for (int i = 0; i < amountOfPanels; i ++) {
-            editorPane.setPage(new File(".cache" + SEPARATOR + "Q" + id + ".HTM").toURI().toURL());
             JPanel alternativePanel = new JPanel();
             alternativePanel.setLayout(new BoxLayout(alternativePanel, BoxLayout.X_AXIS));
             JRadioButton radioButton = new JRadioButton();
@@ -166,9 +170,16 @@ public class GUI extends JFrame {
             alternativePanel.add(radioButton);
             alternativePanel.add(Box.createRigidArea(new Dimension(10, 10)));
             alternativePanel.add(label);
-            panel5.add(alternativePanel);
+            panel1.add(alternativePanel);
         }
-        panel2.add(panel5);
+        panel1.setPreferredSize(new Dimension(430, 600));
+        panel.add(Box.createRigidArea(new Dimension(15, 15)));
+        panel.add(scrollPane);
+        panel.add(Box.createRigidArea(new Dimension(15, 15)));
+        panel.add(panel1);
+        panel.add(Box.createRigidArea(new Dimension(15, 15)));
+        panel3.add(panel);
+        panel2.add(panel3);
         SwingUtilities.updateComponentTreeUI(this);
     }
 
@@ -180,19 +191,22 @@ public class GUI extends JFrame {
         @Override
         public void actionPerformed(ActionEvent event) {
             if (event.getSource() == nextButton) {
-                for (JRadioButton radioButton : radioButtonList) {
-                    if (radioButton.isSelected()) {
-                        String answer = radioButton.getText();
-                        System.out.println(answer);
-                        panel2.remove(panel5);
-                        editorPane.setText("<html> </html>");
-                        SwingUtilities.updateComponentTreeUI(GUI.this);
-                        currentQuestionPosi ++;
-                        currentQuestion.setText(String.format("Questão %d de %d", currentQuestionPosi, maxQuestionPosi));
-                        try {
-                            createQuestionComponents();
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                if (isMultipleChoiceQuestion) {
+                    for (int i = 0; i < radioButtonList.size(); i ++) {
+                        if (radioButtonList.get(i).isSelected()) {
+                            String answer = radioButtonList.get(i).getText();
+                            System.out.println(answer);
+                            panel3.removeAll();
+                            panel2.remove(panel3);
+                            currentQuestionPosi ++;
+                            currentQuestion.setText(String.format("Questão %d de %d", currentQuestionPosi, maxQuestionPosi));
+                            SwingUtilities.updateComponentTreeUI(GUI.this);
+                            try {
+                                createQuestionComponents();
+                            }
+                            catch (IOException e) {
+                                System.out.println(e.getMessage());
+                            }
                         }
                     }
                 }
