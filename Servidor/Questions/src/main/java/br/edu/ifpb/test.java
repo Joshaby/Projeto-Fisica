7b26d292fbc02f59858fc221fcc5f2800d8c300b
talisonk <talisonkennedy1@gmail.com>
2020-07-14 12:28:25 -0300

    retorno do server

--- Servidor/Questions/src/main/java/br/edu/ifpb/test.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpb;
-
-public class test {
-    public static void main(String[] args) {
-        QuestionRepository q = new QuestionRepository();
-        q.setQuestions(4, 10, 3);
-    }
-}
--- Servidor/ServerLogic/src/main/java/br/edu/ifpb/test.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpb;
-
-public class test {
-    public static void main(String[] args) {
-        ServerConnection server = new ServerConnection();
-
-    }
-}
