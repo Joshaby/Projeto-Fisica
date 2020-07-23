7315c9fcae4af1217f04735ecea2998cb334e74c
talisonk <talisonkennedy1@gmail.com>
2020-07-23 15:24:05 -0300

    entrada do ServerLogic

---
+++ Servidor/Groups/src/main/java/br/edu/ifpb/User_IF.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface User_IF extends Remote {
+
+    void registerGroups(Map<String, List<String>> groups, int year) throws RemoteException;
+
+    Map<String, List<String>> getGroupsMAP() throws RemoteException;
+
+    Map<String, List<String>> getGroupByNameMap(String name) throws RemoteException;
+
+    List<String> realocateGroup(int round) throws RemoteException;
+
+
+
+}
