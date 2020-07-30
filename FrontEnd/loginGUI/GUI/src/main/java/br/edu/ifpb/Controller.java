5dfb869570fb311b62815f88e3444d10d91ba43a
talisonk <talisonkennedy1@gmail.com>
2020-07-30 18:40:17 -0300

    Inicio da segunda etapa do projeto, a união faz a força

--- FrontEnd/loginGUI/GUI/src/main/java/br/edu/ifpb/Controller.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-
-public class Controller {
-}
--- FrontEnd/loginGUI/GUI/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module br.edu.ifpb {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens br.edu.ifpb;
-}
