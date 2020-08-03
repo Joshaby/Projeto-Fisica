6b9232b07f0092bdb9a2b34501bb5273eeb55c4b
vnioliveira <vo7775832@gmail.com>
2020-08-03 18:26:26 -0300

    Setores Alterados:-

--- FrontEnd/Cliente/Login/src/main/java/br/edu/ifpb/App.java
+++ FrontEnd/Cliente/Login/src/main/java/br/edu/ifpb/App.java
@@ -1,13 +1,29 @@
 package br.edu.ifpb;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
 
+    private Stage stage;
+
+    private Scene scene;
+
+    public App() throws IOException {
+        Parent loginParent = new FXMLLoader(App.class.getResource("Login.fxml")).load();
+        this.scene = new Scene(loginParent);
+    }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
 
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
